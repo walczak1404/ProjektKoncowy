cb6993d12cc99e44fa878c6d8c6aeba4ac01071f
Dawid Walczak <dawidwalczak1404@gmail.com>
2024-05-31 00:34:07 +0200

    create scroll-snap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { SectionLayoutComponent } from './components/shared/section-layout/section-layout.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
-  declarations: [
+  declarations: [ 
     AppComponent,
-    HeroComponent
+    HeroComponent,
+    AboutMeComponent,
+    SectionLayoutComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/shared/section-layout/section-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-section-layout',
+  templateUrl: './section-layout.component.html',
+  styleUrl: './section-layout.component.scss'
+})
+export class SectionLayoutComponent {
+  @Input("title") title: string;
+
+}
